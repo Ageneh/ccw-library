741df3861117a223b931c99508ff0c74d777a826
Henock Arega <harega@seibert-media.net>
2023-05-10 16:28:48 +0200

    Initial vue.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+	<div id='app'>
+		<h1>Chapel Library</h1>
+		<div class='table'>
+      <h1>Chapel Library</h1>
+		</div>
+	</div>
 </template>
 
-<style lang="less">
+<style lang='less'>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,77 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require("@vue/cli-service")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const webpack = require("webpack");
+
+const manifestJson = {
+  name: "Chapel Library",
+  "short-name": "CCW Library",
+  background_color: "#ebebeb",
+  description: "Willkomen in der CCW Library.",
+  shortcuts: [
+    {
+      "name": "Bücher",
+      "url": "/books",
+      "description": "Heutiger Vers.",
+    },
+    {
+      "name": "Info",
+      "url": "/about",
+    },
+  ],
+  theme_color: "#ebebeb",
+  start_url: "/",
+  scope: "/",
+  display: "standalone",
+  // display: "minimal-ui",
+}
+
+const isDev = process.env.NODE_ENV === "development"
+const isAnalyze = false
+
+process.env.VUE_APP_VERSION = require("./package.json").version
+
+const devPlugins = [
+  isAnalyze
+    ? new BundleAnalyzerPlugin({
+      analyzerPort: 7777,
+      openAnalyzer: process.env?.DEV_ANALYZE,
+      logLevel: "error",
+    })
+    : undefined,
+]
+  .filter(i => !!i)
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      ...isDev ? devPlugins : [],
+    ],
+  },
+  pwa: {
+    name: manifestJson.name,
+    display: manifestJson.display,
+    themeColor: manifestJson.theme_color,
+    msTileColor: "#ebebeb",
+    appleMobileWbeAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#ebebeb",
+    workboxOptions: {
+      exclude: [/_redirect/, /\.map$/, /_headers/],
+      clientsClaim: true,
+    },
+    manifestOptions: manifestJson,
+    // iconPaths: { TODO
+    //   faviconSVG: "favicon.svg",
+    //   favicon32: "img/icons/favicon-32x32.png",
+    //   favicon16: "img/icons/favicon-16x16.png",
+    //   appleTouchIcon: "img/icons/apple-touch-icon-180x180.png",
+    //   maskIcon: "img/icons/safari-pinned-tab.svg",
+    //   msTileImage: "img/icons/mstile-150x150.png",
+    // },
+  },
+  devServer: {
+    proxy: "http://localhost:8888",
+  },
+  crossorigin: "anonymous",
+  pluginOptions: {},
 })
